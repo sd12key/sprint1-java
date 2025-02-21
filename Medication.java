b50933f34e52267552355349e783be327f71a339
Eugene Kepich <166713967+sd12key@users.noreply.github.com>
2025-02-21 00:23:03 -0330

    + Medication class

---
+++ Medication.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+
+public class Medication {
+    private final int id;
+    private String name;
+    private double dose;
+    private int quantity_in_stock;
+    private LocalDate expiry_date;
+
+    public Medication(int id, String name, double dose, int quantity_in_stock, LocalDate expiry_date) {
+        this.id = id;
+        this.name = name;
+        this.dose = dose;
+        this.quantity_in_stock = quantity_in_stock;
+        this.expiry_date = expiry_date; 
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getDose() {
+        return this.dose;
+    }
+
+    public int getQuantityInStock() {
+        return this.quantity_in_stock;
+    }
+
+    public LocalDate getExpiryDate() {
+        return this.expiry_date;
+    }
+
+    // private setter method to change the name of the medication
+    // in a real-world scenario, the name of a medication should not be changed
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDose(double dose) {
+        this.dose = dose;
+    }
+
+    public void setQuantityInStock(int quantity_in_stock) {
+        this.quantity_in_stock = quantity_in_stock;
+    }
+
+    public void setExpiryDate(LocalDate expiry_date) {
+        this.expiry_date = expiry_date;
+    }
+
+    public String toString() {
+        return "Medication[" + this.id + ", " + this.name + ", " + this.dose + "mg, Stock: " 
+                + this.quantity_in_stock + ", Expiry: " + this.expiry_date + "]";
+    }
+}
